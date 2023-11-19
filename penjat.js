236bd990ee6106f010707395f8385a2c6a2dc9ac
IticMarsh <2023_arnau.marsino@iticbcn.cat>
2023-11-19 16:07:31 +0100

    versió2

--- penjat.js
+++ penjat.js
@@ -1,4 +1,4 @@
-//definicio de variables
+// Definició de variables
 var palabraSecreta = "";
 var palabraAdivinada = "";
 var intentosMaximos = 6;
@@ -7,107 +7,139 @@ var letrasFalladas = [];
 var partidasJugadas = 0;
 var partidasGanadas = 0;
 var partidasPerdidas = 0;
+//funcio per reiniciar estadistiques
+function reiniciarestadisticas() {
+  intentosFallados = 0;
+  letrasFalladas = [];
+  partidasJugadas = 0;
+  partidasGanadas = 0;
+  partidasPerdidas = 0;
 
-//funcio mostrar estadistiques 
+  alert("Les estadístiques han estat reiniciades");
+}
+
+function bloquearBoton(letra) {
+  var botones = document.querySelectorAll("button");
+  botones.forEach(function (button) {
+    if (button.innerHTML === letra) {
+        //desactivar botó + canviar css color del mateix
+      button.disabled = true;
+      button.classList.add("usado");
+    }
+  });
+}
+
+// Funció per mostrar estadístiques
 function mostrarEstadisticas() {
-    console.log("Total de partides: " + partidasJugadas);
-    console.log("Partides guanyades (" + ((partidasGanadas / partidasJugadas) * 100).toFixed(2) + "%): " + partidasGanadas);
-    console.log("Partides perdudes (" + ((partidasPerdidas / partidasJugadas) * 100).toFixed(2) + "%): " + partidasPerdidas);
-    mostrarMenu();
+  var estadisticas = "Total de partides: " + partidasJugadas + "\n";
+  estadisticas +=
+    "Partides guanyades (" +
+    ((partidasGanadas / partidasJugadas) * 100).toFixed(2) +
+    "%): " +
+    partidasGanadas +
+    "\n";
+  estadisticas +=
+    "Partides perdudes (" +
+    ((partidasPerdidas / partidasJugadas) * 100).toFixed(2) +
+    "%): " +
+    partidasPerdidas;
+
+  var nuevaVentana = window.open("", "_blank", "width=400, height=300");
+  nuevaVentana.document.write("<pre>" + estadisticas + "</pre>");
+  nuevaVentana.document.close();
 }
 
-//funcio principal 
-function mostrarMenu() {
-   
-     
-    var opcion = prompt("1. Iniciar un joc\n2. Mostra estadístiques\n3. Sortir");
-   
-//switchcase
-    switch (opcion) {
-        case "1":
-            intentosFallados = 0
-            palabraSecreta = prompt("Introdueix una paraula secreta:").toLowerCase();
-            //emplenar de barra baixa la paraula
-            palabraAdivinada = "_".repeat(palabraSecreta.length);
-            //crida la funcio
-            adivinarLetra();
-            break;
-        case "2":
-            mostrarEstadisticas();
-            break;
-        case "3":
-            alert("Fins aviat!");
-            break;
-        
-        
-    }
+// Funció principal
+function IniciarPartida() {
+  var botonesUsados = document.querySelectorAll(".usado");
+  botonesUsados.forEach(function (button) {
+    button.disabled = false;
+    button.classList.remove("usado");
+  });
+  //reiniciar valors de les variables al començar una nova partida
+  letrasFalladas = [];
+  intentosFallados = 0;
+  palabraSecreta = prompt("Introdueix una paraula secreta:").toLowerCase();
+  palabraAdivinada = "_".repeat(palabraSecreta.length);
+  adivinarLetra();
 }
 
-function adivinarLetra() {
-    var letra = prompt("Introdueix una lletra:").toLowerCase();
-//comprovació que sigui lletra valida i un sol caractes
-    if (letra.length !== 1 || !esLetra(letra)) {
-        console.log("Error: Introdueix una sola lletra vàlida.");
-        adivinarLetra();
-        return;
-    }
-
-    if (palabraSecreta.includes(letra)) {
-        //bucle per a comprovar si la paraula conté la lletra dle usuari i en quina posicio
-        for (var i = 0; i < palabraSecreta.length; i++) {
-            if (palabraSecreta[i] === letra) {
-                //cridar funcio que remplaça la lletra a la posicio adecuada
-                palabraAdivinada = reemplazarLetra(palabraAdivinada, i, letra);
-            }
-        }
-    } else {
-        //agregar al array de lletres fallades la lletra i sumar intent
-        letrasFalladas.push(letra);
-        intentosFallados++;
-    }
-//mostrar el progres de lletres fallades
-    mostrarProgreso();
-//si el usuari segueix viu segueix mostrant el input de lletra
-    if (palabraAdivinada !== palabraSecreta && intentosFallados < intentosMaximos) {
-        adivinarLetra();
-    }
+function sortir() {
+  alert("Fins aviat!");
 }
-//remplaçar lletra
+
+function adivinarLetra(letra) {
+  var imatgePenjat = document.getElementById("imatgePenjat");
+    //funcio per trobar les lletres de la paraula
+  if (palabraSecreta.includes(letra)) {
+    for (var i = 0; i < palabraSecreta.length; i++) {
+      if (palabraSecreta[i] === letra) {
+        palabraAdivinada = reemplazarLetra(palabraAdivinada, i, letra);
+      }
+    }
+  } else {
+    //si es falla la lletra es posa la imatge de l'estat del penjat i es suma un intent
+    letrasFalladas.push(letra);
+    imatgePenjat.src = "penjat_" + intentosFallados + ".png";
+    intentosFallados++;
+  }
+  bloquearBoton(letra);
+  mostrarProgreso();
+}
+
+// Funció para canviar lletra
 function reemplazarLetra(cadena, indice, letra) {
-    return cadena.substr(0, indice) + letra + cadena.substr(indice + 1);
+  return cadena.substr(0, indice) + letra + cadena.substr(indice + 1);
 }
-//funcio per comprovar si el caractes es una lletra
+
+// Función para comprobar si el carácter es una letra
 function esLetra(caracter) {
-    return /^[a-z]$/.test(caracter);
+  return /^[a-z]$/.test(caracter);
 }
-//funcio per mostrar el progres o si ha acabat el joc
+
+// Funció per mostrar el progres o si s'ha acabat el joc
 function mostrarProgreso() {
-    console.log("Paraula: " + palabraAdivinada);
-    console.log("Lletres fallades " + intentosFallados + "/" + intentosMaximos + ": " + letrasFalladas.join(", "));
+  var jocPenjat = document.getElementById("jocPenjat");
+  jocPenjat.innerHTML = "Paraula: " + palabraAdivinada + "<br>";
+  jocPenjat.innerHTML +=
+    "Lletres fallades " +
+    (intentosFallados - 1) +
+    "/" +
+    intentosMaximos +
+    ": " +
+    letrasFalladas.join(", ") +
+    "<br>";
 
-    if (palabraAdivinada === palabraSecreta) {
-        console.log("Felicitats! Has encertat la paraula: " + palabraSecreta);
-        partidasGanadas++;
-        partidasJugadas++;
-        mostrarMenu();
-
-    } else if (intentosFallados === intentosMaximos) {
-        console.log("Has mort penjat. La paraula era: " + palabraSecreta);
-        partidasPerdidas++;
-        partidasJugadas++;
-        mostrarMenu();
-       
-    }
+  if (palabraAdivinada === palabraSecreta) {
+    jocPenjat.innerHTML +=
+      "Felicitats! Has encertat la paraula: " + palabraSecreta + "<br>";
+    partidasGanadas++;
+    partidasJugadas++;
+  } else if (intentosFallados - 1 === intentosMaximos) {
+    jocPenjat.innerHTML +=
+      "Has mort penjat. La paraula era: " + palabraSecreta + "<br>";
+    partidasPerdidas++;
+    partidasJugadas++;
+  }
 }
-mostrarMenu()
 
+// Funció per mostrar el teclat
+function mostrarAbecedario() {
+  var abecedario = "abcdefghijklmnopqrstuvwxyz"; // Caracteres permitidos
+  var abecedarioHTML = "";
 
+  for (var i = 0; i < abecedario.length; i++) {
+    var letra = abecedario[i];
+    abecedarioHTML +=
+      "<button onclick=\"adivinarLetra('" +
+      letra +
+      '\')" class="usado">' +
+      letra +
+      "</button>";
+  }
 
+  document.getElementById("abecedari").innerHTML = abecedarioHTML;
+}
 
-
-
-
-
-
-
-
+// Llamada a la función para mostrar el abecedario
+mostrarAbecedario();
